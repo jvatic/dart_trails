15cc88c90704c679a0a194cac23f46b413839ec7
Kelsey Judson <kelseyjudson@gmail.com>
2013-06-22 01:15:23 +1200

    Decide to remove alternative example code.

--- config/initializers/dart_trails.rb
+++
@@ -1,4 +0,0 @@
-# This is handled by DartTrails::Engine, however I am leaving this in place as
-# an alternative example.
-#
-# Rails.application.assets.register_engine('.dart', DartTrails::Tilt::Template)
